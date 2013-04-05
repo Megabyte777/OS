55cad0d4f95083c11189b69d0d9a57b40eb16f5b
Denis Mekhanikov <Megabyte777@bigmir.net>
2013-04-05 04:08:53 +0400

    Reading k bytes instead of k+1

--- dupme/main.c
+++ dupme/main.c
@@ -7,17 +7,21 @@ int get_next_token(char *read_buffer, char *write_buffer, const int buf_size,
         int *len, int *available)
 {
     int long_string = 0;
+    int first_overflow = 0;
     int ready = 0;
-    while (ready == 0)
+    for (;;)
     {
         int ret = read(0, read_buffer + *len, buf_size - *len);
         if (ret <= 0)
         {
-            if ((*len > 0) && (long_string == 0) && 
-                    ((*len < buf_size) || (read_buffer[(*len) - 1] == '\n')))
+            memcpy(write_buffer, read_buffer, (*len) * sizeof(char));
+            if (first_overflow != 0)
             {
-                memcpy(write_buffer, read_buffer, (*len) * sizeof(char));
-                if (write_buffer[(*len) - 1] != '\n')
+                *available = buf_size + 1;
+            }
+            else if (long_string == 0)
+            {
+                if (((*len) != 0) && (write_buffer[(*len) - 1] != '\n'))
                 {
                     write_buffer[*len] = '\n';
                     (*len)++;
@@ -27,56 +31,61 @@ int get_next_token(char *read_buffer, char *write_buffer, const int buf_size,
             return ret;
         }
         (*len) += ret;
-        int start = 0;
-        int found_nl = 0;
+        if (first_overflow == 1)
+        {
+            if (read_buffer[0] == '\n')
+            {
+                *available = buf_size + 1;
+                memmove(read_buffer, read_buffer + 1, ((*len) - 1) * sizeof(char));
+                (*len)--;
+                return 1;
+            }
+            else
+            {
+                long_string = 1;
+                first_overflow = 0;
+            }
+        }
+        int end = 0;
         if (long_string != 0)
         {
-            while ((found_nl == 0) && (start < *len))
+            while ((end < (*len - 1)) && (read_buffer[end] != '\n'))
             {
-                if (read_buffer[start] == '\n')
-                {
-                    found_nl = 1;
-                }
-                start++;
+                end++;
             }
-            if (start < *len)
+            if (read_buffer[end] == '\n')
             {
                 long_string = 0;
             }
+            end++;
+            (*len) -= end;
+            memmove(read_buffer, read_buffer + end, (*len) * sizeof(char));
         }
-        found_nl = 0;
-        int end = start;
-        while ((found_nl == 0) && (end < *len))
+        end = 0;
+        while ((end < *len) && (read_buffer[end] != '\n'))
         {
-            if (read_buffer[end] == '\n') 
-            {
-                found_nl = 1;
-            }
             end++;
         }
-        if ((found_nl != 0) || (start != 0))
-        {
-            if (found_nl != 0)
-            {
-                *available = end - start;
-                memcpy(write_buffer, read_buffer + start, 
-                        *available * sizeof(char));
-                start = end;
-                ready = 1;
-            }
-            (*len) -= start;
-            memmove(read_buffer, read_buffer + start, (*len) * sizeof(char));
-        }
-        else
+        if (end >= *len)
         {
             if (*len == buf_size)
             {
-                long_string = 1;
+                first_overflow = 1;
+                memcpy(write_buffer, read_buffer, (*len) * sizeof(char));
+                write_buffer[buf_size] = '\n';
                 *len = 0;
             }
         }
+        else
+        {
+            long_string = 0;
+            *available = end + 1;
+            memcpy(write_buffer, read_buffer, (*available) * sizeof(char));
+            (*len) -= end + 1;
+            memmove(read_buffer, read_buffer + end + 1, (*len) * sizeof(char));
+            return 1;
+        }
     }
-    return 1;
 }
 
 void print(char *write_buffer, int *available, int count)
@@ -110,14 +119,13 @@ int main(int argc, char *argv[])
             buf_size *= 10;
             buf_size += argv[1][i] - '0';
         }
-        buf_size++;
     }
     else
     {
         buf_size = 5;
     }
     char *read_buffer = malloc(buf_size);
-    char *write_buffer = malloc(buf_size);
+    char *write_buffer = malloc(buf_size + 1);
     int len = 0;
     int available = 0;
     while (get_next_token(read_buffer, write_buffer, buf_size, &len, &available) > 0)
